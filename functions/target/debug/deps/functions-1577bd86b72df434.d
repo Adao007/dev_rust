337af4281edbbeb31a5783d284f00f12e56ad871
Tony <anthonydaoy@gmail.com>
2023-09-28 16:39:17 -0700

    "finish 3.2 will message next update"

---
+++ functions/target/debug/deps/functions-1577bd86b72df434.d
@@ -0,0 +1,5 @@
+/Users/anthonydao/dev_rust/functions/target/debug/deps/functions-1577bd86b72df434.rmeta: src/main.rs
+
+/Users/anthonydao/dev_rust/functions/target/debug/deps/functions-1577bd86b72df434.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/deps/functions-3747df4dd8535ab9.d
@@ -0,0 +1,5 @@
+/Users/anthonydao/dev_rust/functions/target/debug/deps/functions-3747df4dd8535ab9.rmeta: src/main.rs
+
+/Users/anthonydao/dev_rust/functions/target/debug/deps/functions-3747df4dd8535ab9.d: src/main.rs
+
+src/main.rs:
