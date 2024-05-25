3c7a51306ceef9d0efea51a5459ebc4c2048ea3a
Tony <anthonydaoy@gmail.com>
2024-05-25 02:10:21 -0700

    completed medianmode project

--- chp8_projects/median_mode/src/main.rs
+++ chp8_projects/median_mode/src/main.rs
@@ -9,55 +9,58 @@ use std::collections::HashMap;
 
 fn main() {
     let mut rng = rand::thread_rng();
-
-    // Initialize List
     let mut list: [u32; 10] = [0; 10];
     let mut vector: Vec<u32> = Vec::new();
     let mut frequency: HashMap<u32, i32> = HashMap::new();
-
+    
+    // Fills list with random ints between 0 and 100
     for i in 0..list.len() {
         list[i] = rng.gen_range(0..100);
     }
 
     copy_list(&mut list, &mut vector);
     println!("given list: {:?}" ,vector);
-    
     vector.sort();
-    
     println!("after sorted: {:?}", vector);
-    median(&mut vector);
     
+    for element in &mut vector {
+        let amount = frequency.entry(*element).or_insert(0);
+        *amount += 1;
+    }
+
+    median(&mut vector);
+    mode(&mut frequency); 
 }
 
-// function to copy array into vector
+// Function to copy array into vector
 fn copy_list(x: &mut [u32; 10], v: &mut Vec<u32>) {
     for number in x {
         v.push(*number);
     }
 }
 
-//function for printing out the median
+// Function for printing out the median
 fn median(v: &mut Vec<u32>) {
     let middle = (v.len() + 1) / 2;
     println!("Median: {:?}", v[middle]); 
 }
 
-//function to insert elements into HashMap
-fn insert_elements(v: &mut Vec<u32>, hm: &mut HashMap<u32, i32>) {
-    for element in v {
-   println!("{}" ,vector);
-        
+// Returns the mode of the list
+fn mode(hm: &mut HashMap<u32, i32>) {
+    let mut frequency = 0;
+    let mut mode: u32 = 0 ;
+
+    for (key,value) in hm {
+        if frequency <= *value {
+            frequency = *value;
+            mode = *key; 
+        }
+    }
+    
+    if mode == 1 {
+        println!("Mode: {}", mode);
+    }
+    else {
+        println!("No mode exists!");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- chp8_projects/median_mode/target/debug/median_mode.d
+++ chp8_projects/median_mode/target/debug/median_mode.d
@@ -1,1 +1,1 @@
-/home/tony_ubuntu/dev_rust/chp8_projects/median_mode/target/debug/median_mode: /home/tony_ubuntu/dev_rust/chp8_projects/median_mode/src/main.rs
+/Users/anthonydao/dev_rust/chp8_projects/median_mode/target/debug/median_mode: /Users/anthonydao/dev_rust/chp8_projects/median_mode/src/main.rs
