4ba4ac57be5424bf11f446f44da834bc3abb3d2d
Anthony Dao <anthonydaoy@gmail.com>
2024-08-07 12:59:58 -0700

    revised change-making project

--- csudh_rust/coin_change_problem/src/main.rs
+++ csudh_rust/coin_change_problem/src/main.rs
@@ -1,22 +1,22 @@
-use std::cmp;
+use std::cmp;                           // Used for Min function
 
 fn change_making(coins: &[usize], n: usize) -> usize {
-    let m = coins.len();
-    let mut ways = vec![0; n + 1];
+    let c = coins.len();                // Variable to keep track of length of Coins array
+    let mut min = vec![0; n + 1];       // Creates new array to keep track of min coins to return change
 
-    for i in 1..=n {
-        let mut temp = usize::MAX;
-        let mut j = 0;
+    for i in 1..=n {                    // For loop 1 to change amount
+        let mut temp = usize::MAX;      // Temp is set to largest usize value
+        let mut j = 0;                  // Used for while loop increment
 
-        while j < m && i >= coins[j] {
-            temp = cmp::min(ways[i - coins[j]], temp);
+        while j < c && i >= coins[j] {
+            temp = cmp::min(min[i - coins[j]], temp);
             j += 1;
         }
 
-        ways[i] = temp + 1;
+        min[i] = temp + 1;
     }
 
-    ways[n]
+    min[n]
 }
 
 fn main() {
@@ -30,25 +30,29 @@ fn main() {
     
     // Test Case 1: 
     let mut time_before = now.elapsed();
-    println!("Denomiation One, Change-Making: {}", change_making(&denomiation_one, 19));
+    println!("Denomiation One: (1, 3, 4), Minimum Coins needed to return change of 19: {}"
+             , change_making(&denomiation_one, 19));
     let mut timer_after = now.elapsed();
     println!("Time elapsed: {:.2?}", (timer_after - time_before));  
 
     // Test Case 2:
     time_before = now.elapsed();
-    println!("Denomiation Two, Change-Making: {}", change_making(&denomiation_two, 77));
+    println!("Denomiation Two: (1, 5, 10, 20, 25), Minimum Coins needed to return change of 77: {}"
+             , change_making(&denomiation_two, 77));
     timer_after = now.elapsed();
     println!("Time elapsed: {:.2?}", (timer_after - time_before)); 
     
     // Test Case 3:
     time_before = now.elapsed();
-    println!("Denomiation Three, Change-Making: {}", change_making(&denomiation_three, 25));
+    println!("Denomiation Three: (1, 4, 7, 11), Minimum Coins needed to return change of 25: {}"
+             , change_making(&denomiation_three, 25));
     timer_after = now.elapsed();
     println!("Time elapsed: {:.2?}", (timer_after - time_before)); 
     
     // Test Case 4:
     time_before = now.elapsed();
-    println!("Denomiation Four, Change-Making: {}", change_making(&denomiation_four, 24));
+    println!("Denomiation Four: (1, 5, 7, 13), Minimum Coins needed to return change of 24: {}"
+             , change_making(&denomiation_four, 24));
     timer_after = now.elapsed(); 
     println!("Time elapsed: {:.2?}", (timer_after - time_before)); 
 }
